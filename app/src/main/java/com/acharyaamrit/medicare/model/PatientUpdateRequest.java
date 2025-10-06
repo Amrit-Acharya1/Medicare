115b624129c63a54a7ea15f6f08ead6172e70bcd
Amrit Acharya <acharyaamrit47@gmail.com>
2025-10-06 22:29:45 +0545

    make patientUpdateResponse for updating pateien

---
+++ app/src/main/java/com/acharyaamrit/medicare/model/PatientUpdateRequest.java
@@ -0,0 +1,70 @@
+package com.acharyaamrit.medicare.model;
+
+public class PatientUpdateRequest {
+    String name;
+    String address;
+    String dob;
+    int gender;
+    String blood_group;
+    String emergency_contact;
+
+    public PatientUpdateRequest(String name, String address, String dob, int gender, String blood_group, String emergency_contact) {
+        this.name = name;
+        this.address = address;
+        this.dob = dob;
+        this.gender = gender;
+        this.blood_group = blood_group;
+        this.emergency_contact = emergency_contact;
+    }
+
+    public PatientUpdateRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getBlood_group() {
+        return blood_group;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
+
+    public String getEmergency_contact() {
+        return emergency_contact;
+    }
+
+    public void setEmergency_contact(String emergency_contact) {
+        this.emergency_contact = emergency_contact;
+    }
+}
