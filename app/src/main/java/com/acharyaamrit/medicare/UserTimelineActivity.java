f8cf5871ef28d948d7089664aafd9f1e37adddd4
Kritish11 <kritishdevkota11@gmail.com>
2025-10-08 21:18:57 +0545

    user timeline main acctivity created

---
+++ app/src/main/java/com/acharyaamrit/medicare/UserTimelineActivity.java
@@ -0,0 +1,32 @@
+package com.acharyaamrit.medicare;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class UserTimelineActivity extends AppCompatActivity {
+
+    private CardView backButtonCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_timeline);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, 0, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        backButtonCard = findViewById(R.id.backButtonCard);
+
+        backButtonCard.setOnClickListener(v -> {finish();});
+
+
+    }
+}
